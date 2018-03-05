72b5d264cfe0a8ed85046eba42eb59558297c86b
sch3lp <sch3lp@gmail.com>
2018-03-05 13:41:07 +0100

    junit5 doesn't need public keyword

--- src/test/java/be/swsb/coderetreat/HelloTest.java
+++ src/test/java/be/swsb/coderetreat/HelloTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class HelloTest {
 
     @Test
-    public void greet_ShouldReturnWorld() throws Exception {
+    void greet_ShouldReturnWorld() throws Exception {
         assertThat(new Hello().greet()).isEqualTo("World!");
     }
 }
