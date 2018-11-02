c9955058afb243e5d4ff89593f20cea30edda740
sch3lp <sch3lp@gmail.com>
2018-11-02 14:48:19 +0100

    step 2: fix unit test

--- src/main/java/be/swsb/coderetreat/Position.java
+++ src/main/java/be/swsb/coderetreat/Position.java
@@ -1,5 +1,7 @@
 package be.swsb.coderetreat;
 
+import java.util.Objects;
+
 public class Position {
     private final int x;
     private final int y;
@@ -15,5 +17,19 @@ public class Position {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/be/swsb/coderetreat/Rover.java
+++ src/main/java/be/swsb/coderetreat/Rover.java
@@ -2,10 +2,6 @@ package be.swsb.coderetreat;
 
 public class Rover {
 
-    public String greet() {
-        return "World!";
-    }
-
     public Position getPosition() {
         return new Position(0,0);
     }
