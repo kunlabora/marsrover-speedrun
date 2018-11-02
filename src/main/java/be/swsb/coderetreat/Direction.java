bdda28aaa1736f80737bc458965b8a63115ee8c6
sch3lp <sch3lp@gmail.com>
2018-11-02 15:54:30 +0100

    step 26: implemented ALL counterclockwise

--- src/main/java/be/swsb/coderetreat/Direction.java
+++ src/main/java/be/swsb/coderetreat/Direction.java
@@ -1,12 +1,23 @@
 package be.swsb.coderetreat;
 
+import java.util.HashMap;
+
 public enum Direction {
     NORTH,
     EAST,
     SOUTH,
     WEST;
 
+    private static HashMap<Direction, Direction> counterClockwiseMap = new HashMap<>();
+
+    static {
+        counterClockwiseMap.put(NORTH, Direction.WEST);
+        counterClockwiseMap.put(EAST, Direction.NORTH);
+        counterClockwiseMap.put(SOUTH, Direction.EAST);
+        counterClockwiseMap.put(WEST, Direction.SOUTH);
+    }
+
     public Direction counterClockwise() {
-        return null;
+        return counterClockwiseMap.get(this);
     }
 }
